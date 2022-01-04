2d14c2ac312f99183bd2e1c76eecce8ccedd13fa
Matías Fuentes <matiasf.cortez@gmail.com>
2022-01-03 23:53:25 -0300

    Added mobile image carrusel for product

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -3,6 +3,9 @@ import ImageViewer from '../components/image-viewer/ImageViewer.component'
 import ImageModal from '../components/ImageModal/ImageModal.component'
 import ProductDescription from '../components/product-description/product-description.component'
 import './ProductPage.styles.scss'
+import {ReactComponent as PrevIcon} from '../images/icon-previous.svg'
+import {ReactComponent as NextIcon} from '../images/icon-next.svg'
+import CustomButton from '../components/CustomButton/CustomButton.component'
 const ProductPage = () => {
 
     const [activeModal, setActiveModal] = useState(true)
@@ -15,8 +18,13 @@ const ProductPage = () => {
         
         <div className='product-page'>
             <ImageModal setImage={setImage} image={image} setActiveModal={setActiveModal} activeModal={activeModal}/>
+            
+            <CustomButton onClick={()=>{setImage(image===0?image:image-1)}} className='prev-picture-button'><PrevIcon></PrevIcon></CustomButton>
 
                 <ImageViewer setImage={setImage} image={image}  setActiveModal={setActiveModal} activeModal={activeModal}></ImageViewer>
+                
+            <CustomButton onClick={()=>{setImage(image===3?image:image+1)}} className='next-picture-button'><NextIcon></NextIcon></CustomButton>
+
 
             <ProductDescription></ProductDescription>
         </div>
